3f17ad703e44c712a01b1ac4df3d70d4c87c2212
AlexJozwicki <contact@alexandrejozwicki.com>
2015-03-24 15:58:49 +0100

    Action in ES6

--- src/Action.js
+++ src/Action.js
@@ -7,47 +7,47 @@ var Publisher = require('./Publisher');
  * @constructor
  * @extends {Publisher}
  */
-var Action = function (definition) {
-    definition = definition || {};
-    if (!_.isObject(definition)) {
-      definition = { actionType: definition };
+class Action extends Publisher {
+    constructor(definition) {
+        super( this );
+
+        definition = definition || {};
+        if (!_.isObject(definition)) {
+          definition = { actionType: definition };
+        }
+
+        this._isAction = true;
+        this.actionType = 'action';
+        this.eventType = 'event';
+        this.asyncResult = !!definition.asyncResult;
+
+        this.children = definition.children || [];
+        if (this.asyncResult) {
+            this.children.push('completed', 'failed');
+        }
+
+        delete definition.children;
+        _.extend(this, definition);
+        this.createChildActions();
+
+        var trigger = definition.sync ? this.triggerSync : this.trigger;
+        var functor = trigger.bind(this);
+        functor.__proto__ = this;
+        return functor;
     }
 
-    Publisher.call(this);
 
-    this._isAction = true;
-    this.actionType = 'action';
-    this.eventType = 'event';
-    this.asyncResult = !!definition.asyncResult;
-
-    this.children = definition.children || [];
-    if (this.asyncResult) {
-        this.children.push('completed', 'failed');
+    /**
+     * @protected
+     */
+    createChildActions() {
+        var children = this.children;
+        for (var i = 0; i < children.length; ++i) {
+            var name = children[i];
+            this[name] = new Action({ actionType: name });
+        }
     }
-
-    delete definition.children;
-    _.extend(this, definition);
-    this.createChildActions();
-
-    var trigger = definition.sync ? this.triggerSync : this.trigger;
-    var functor = trigger.bind(this);
-    functor.__proto__ = this;
-    return functor;
-};
-
-_.inherits(Action, Publisher);
-
-
-/**
- * @protected
- */
-Action.prototype.createChildActions = function () {
-    var children = this.children;
-    for (var i = 0; i < children.length; ++i) {
-        var name = children[i];
-        this[name] = new Action({ actionType: name });
-    }
-};
+}
 
 
 module.exports = Action;
